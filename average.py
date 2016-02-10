33db791467cec920f672f096315af7006e647042
Andrew McLeod <amcleod@astro.ex.ac.uk>
2016-02-10 15:18:03 +0000

    average.py now deals with multiple (blank-line delimited) blocks of data

--- average.py
+++ average.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
-from __future__ import division
-range = xrange
+#!/usr/bin/env python3
 
 import numpy as np
 import sys
 import os
 import string
+from io import StringIO, BytesIO
 
 output_choices = sys.argv[1:]
 N_outputs = len(output_choices)
@@ -26,22 +23,64 @@ else:
     print('output to average.dat')
     guess_base_name = 'average.dat'
 
-full_data = None
-
-data_sets = []
+filesets = []
+nblocks = None
 
 for i, output_choice in enumerate(output_choices):
-    data = np.loadtxt(output_choice)
-    if full_data is None:
-        N_x, N_y = data.shape
-        full_data = np.zeros((N_x, N_y, N_outputs+1))
+    # Load blocks of text from file [i]
+    line_blocks = []
+    data_blocks = []
+    with open(output_choice, 'rt') as f:
+        string_data = f.readlines()
+    current_line_block = []
+    for line in string_data:
+        if line == '\n':
+            if current_line_block:
+                line_blocks.append(current_line_block)
+            current_line_block = []
+        else:
+            current_line_block.append(line)
+
+    # Read block of text into numpy array
+    for line_block in line_blocks:
+        if line_block:
+            strbuffer = StringIO(''.join(line_block))
+            data = np.loadtxt(strbuffer)
+            data_blocks.append(data)
+    filesets.append(data_blocks)
+    
+    # Check number of blocks is consistent with previous files
+    if nblocks is None:
+        nblocks = len(data_blocks)
     else:
-        N_x_temp, N_y_temp = data.shape
-        if (N_x != N_x_temp) or (N_y != N_y_temp):
-            raise ValueError('Inconsistent shapes!')
-    full_data[..., i+1] = data
-    full_data[..., 0] += data
+        if nblocks != len(data_blocks):
+            raise ValueError('Inconsistent number of blocks!')
 
-full_data[..., 0] = full_data[..., 0] / float(N_outputs)
+# Allocate space for sum of data in blocks
+full_data = [None]*nblocks
 
-np.savetxt('{}_average.dat'.format(guess_base_name), full_data[..., 0])
+for fileset in filesets:
+    # Loop over each file
+    for i, data in enumerate(fileset):
+        if full_data[i] is None:
+            # Allocate new storage for block
+            N_x, N_y = data.shape
+            full_data[i] = np.zeros((N_x, N_y))
+        else:
+            # Check block shape has not changed
+            N_x, N_y = full_data[i].shape
+            N_x_temp, N_y_temp = data.shape
+            if (N_x != N_x_temp) or (N_y != N_y_temp):
+                raise ValueError('Inconsistent shapes!')
+        full_data[i] += data
+
+output_buffer = BytesIO()
+
+for i in range(nblocks):
+    full_data[i] = full_data[i] / float(N_outputs)
+    np.savetxt(output_buffer, full_data[i])
+    if i != nblocks:
+        output_buffer.write(b'\n')
+
+with open('{}_average.dat'.format(guess_base_name), 'wb') as f:
+    f.write(output_buffer.getvalue())
